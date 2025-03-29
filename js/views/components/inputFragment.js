dbb3ec7cc5482fd65adeb5d0ada323498b1d977f
Eduardo Apen <abjosueapen@gmail.com>
2025-03-29 07:47:14 -0600

    Se agrego titulo para accesibilidad

--- js/views/components/inputFragment.js
+++ js/views/components/inputFragment.js
@@ -15,7 +15,9 @@ const inputNode = async (params) => {
                  <textarea id="inputNode-text"  name="textarea" rows="5" cols="15" placeholder="Ingresa la suma aquí"></textarea>
              </div>
              <div class="input-button-container">
-                <div class="input-button w-56 h-16 my-6 flex justify-center items-start bg-blue-600 rounded-md cursor-pointer" id="input-button" >
+                <div class="input-button w-56 h-16 my-6 flex justify-center items-start bg-blue-600 rounded-md cursor-pointer"
+                 id="input-button"
+                 title = "Click o presione Enter para realizar la operación" >
                     <p class="text-white font-bold text-3xl mt-2 " > Resultado </p>
                 </div>
              </div>
